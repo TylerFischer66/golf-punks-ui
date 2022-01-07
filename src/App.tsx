dec8af67ec8597c60389ce37cb1a22981619af75
methoddev123 <methoddev123@gmail.com>
2022-01-07 14:21:30 -0600

    Testing

--- src/App.tsx
+++ src/App.tsx
@@ -7,16 +7,8 @@ import abi from './GolfPunks.json';
 const golfPunksContractAddress = '0xDDb06a374dc8168D90cD55453993ccA10F739Ac8';
 const provider = ethers.getDefaultProvider();
 console.log(abi);
-const golfPunkMetaData = () => {
-  // ethers.
-  // const contract = new ethers.Contract(
-  //   golfPunksContractAddress,
-  //   abi as any,
-  //   provider
-  // ) as GolfPunksInterface;
-  // console.log(contract);
-  // contract.getName
-};
+fetch('http:locahost:5000/api/token/0').then((data) => console.log(data));
+const golfPunkMetaData = () => {};
 const App = () => {
   return <div className="App">Golf punks</div>;
 };
